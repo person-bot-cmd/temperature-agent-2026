05e047d216f7552f3a6ed545a51f860181da6594
person-bot-cmd <sbaig@alphastar.academy>
2026-04-23 09:00:14 -0700

    Put daily graph first

--- dashboard.py
+++ dashboard.py
@@ -22,8 +22,20 @@ latest_time = df["time"].max()
 last_24h = df[df["time"] >= latest_time - pd.Timedelta(hours=24)]
 last_week = df[df["time"] >= latest_time - pd.Timedelta(days=7)]
 
-#st.subheader("Temperature Over the Last 24 Hours")
-#st.line_chart(last_24h.set_index("time")["temperature_F"])
+st.subheader("Temperature Over the Last 24 Hours")
+
+fig = px.line(
+    last_24h,
+    x="time",
+    y="temperature_F",
+    title="Temperature Trend (Last 24 Hours)"
+)
+
+fig.update_traces(
+    hovertemplate="Time: %{x}<br>Temp: %{y}°F"
+)
+
+st.plotly_chart(fig)
 
 
 
@@ -42,22 +54,7 @@ fig.update_traces(
 
 st.plotly_chart(fig)
 
-st.subheader("Temperature Over the Last 24 Hours")
 
-fig = px.line(
-    last_24h,
-    x="time",
-    y="temperature_F",
-    title="Temperature Trend (Last 24 Hours)"
-)
-
-fig.update_traces(
-    hovertemplate="Time: %{x}<br>Temp: %{y}°F"
-)
-
-st.plotly_chart(fig)
-#st.subheader("Temperature Over the Last 7 Days")
-#st.line_chart(last_week.set_index("time")["temperature_F"])
 
 
 # Find peak and lowest temperatures
