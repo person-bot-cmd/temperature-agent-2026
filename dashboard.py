5ce43b54aa7b16db392ad0d0f53524973ebb5c89
person-bot-cmd <sbaig@alphastar.academy>
2026-04-23 09:21:04 -0700

    Removed key insights

--- dashboard.py
+++ dashboard.py
@@ -73,39 +73,7 @@ latest_time = df["time"].max()
 # Filter last 24 hours
 last_24h = df[df["time"] >= latest_time - pd.Timedelta(hours=24)]
 last_week = df[df["time"] >= latest_time - pd.Timedelta(days=7)]
-'''
-st.subheader("Temperature Over the Last 24 Hours")
 
-fig = px.line(
-    last_24h,
-    x="time",
-    y="temperature_F",
-    title="Temperature Trend (Last 24 Hours)"
-)
-
-fig.update_traces(
-    hovertemplate="Time: %{x}<br>Temp: %{y}°F"
-)
-
-st.plotly_chart(fig)
-
-
-
-st.subheader("Temperature Over the Last 7 Days")
-
-fig = px.line(
-    last_week,
-    x="time",
-    y="temperature_F",
-    title="Temperature Trend (Last 7 Days)"
-)
-
-fig.update_traces(
-    hovertemplate="Time: %{x}<br>Temp: %{y}°F"
-)
-
-st.plotly_chart(fig)
-'''
 st.subheader("Last 24 Hours")
 daily_fig = make_temp_chart(last_24h, "Daily Temperature Trend")
 st.plotly_chart(daily_fig, use_container_width=True)
@@ -123,16 +91,4 @@ min_temp = last_24h["temperature"].min()
 max_row = last_24h[last_24h["temperature"] == max_temp]
 min_row = last_24h[last_24h["temperature"] == min_temp]
 
-st.subheader("Key Insights (Last 24 Hours)")
 
-st.write(f"Highest temperature: {max_temp}")
-st.write(f"Occurred at: {max_row['time'].values[0]}")
-
-st.write(f"Lowest temperature: {min_temp}")
-st.write(f"Occurred at: {min_row['time'].values[0]}")
-
-#st.write("Last 24 hours of data")
-#st.dataframe(last_24h)
-
-#st.write("Raw data preview")
-#st.dataframe(df)
