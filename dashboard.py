6401668f3c315150792536bde5e525f61d13ffb8
person-bot-cmd <sbaig@alphastar.academy>
2026-04-23 09:56:02 -0700

    Changed font to 30

--- dashboard.py
+++ dashboard.py
@@ -45,7 +45,7 @@ def make_temp_chart(data, title):
         "text": title,
         "x": 0.5,
         "xanchor": "center",
-         "font": {"size": 40}
+         "font": {"size": 30}
     },
     xaxis_title="Time",
     yaxis_title="Temperature (°F)",
